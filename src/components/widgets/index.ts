39cf56ae36a2a19000a10128fcb1d9d120ae8c9b
Alberto José <albertorauljose2@gmail.com>
2026-04-10 17:12:33 +0100

    refactor(widgets): add public API for latest widgets components

--- src/components/widgets/index.ts
+++ src/components/widgets/index.ts
@@ -1,2 +1,3 @@
 export { default as QuantitySelector } from './quantity-selector/quantity-selector';
 export { default as CategoryCard } from './category-card/category-card';
+export { default as ResponsiveImage } from './responsive-image/responsive-image';
