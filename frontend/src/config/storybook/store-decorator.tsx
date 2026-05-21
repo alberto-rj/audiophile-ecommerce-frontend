3a5ddc4d794a89831205a1fe0bca675e632a595a
Alberto José <albertorauljose2@gmail.com>
2026-05-21 12:44:19 +0100

    refactor(storybook): update API slice references after service renaming

--- frontend/src/config/storybook/store-decorator.tsx
+++ frontend/src/config/storybook/store-decorator.tsx
@@ -3,9 +3,9 @@ import { Provider } from 'react-redux';
 
 import { authApi } from '@/app/services/auth-api';
 import { cartApi } from '@/app/services/cart-api';
-import { categoryApi } from '@/app/services/categories-api';
+import { categoriesApi } from '@/app/services/categories-api';
 import { ordersApi } from '@/app/services/orders-api';
-import { productApi } from '@/app/services/products-api';
+import { productsApi } from '@/app/services/products-api';
 import { usersApi } from '@/app/services/users-api';
 import { store } from '@/app/store';
 
@@ -13,9 +13,9 @@ export const StoreDecorator: Decorator = (Story, context) => {
   const apiList = [
     authApi,
     cartApi,
-    categoryApi,
+    categoriesApi,
     ordersApi,
-    productApi,
+    productsApi,
     usersApi,
   ];
 
